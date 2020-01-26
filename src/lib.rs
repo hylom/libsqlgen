81aa70ef4db0775c0d65d2fbf231357994b5ecd9
hylom <hylom@hylom.net>
2020-01-26 21:52:21 +0900

    implement generate_limit_clause() and generate_order_by_clause()

--- src/lib.rs
+++ src/lib.rs
@@ -7,33 +7,83 @@ mod tests {
     #[test]
     fn test_generate_where_clause() {
         let mut t = Table::new("test_table");
-        t.add_key("key01", "key01")
-            .add_key("foo", "key02");
+        t.add_key_and_alias("key01", "key01")
+            .add_key_and_alias("key02", "foo");
 
+        // test for '=' operator
         let qs1 = vec![
             Query::from_int("key01", QueryCondition::Equals, 100),
         ];
-        if let Some(clause) = t.generate_where_clause(&qs1) {
+        if let Ok(clause) = t.generate_where_clause(&qs1) {
             assert_eq!(clause.clause, "key01 = ?");
-            if let ParameterType::Int(x) = *clause.parameters[0].value {
-                assert_eq!(x, 100);
-            } else {
-                assert!(false);
-            }
+            assert_eq!(*clause.parameters[0].value, ParameterType::Int(100));
         } else {
             assert!(false);
         }
 
+        // test for '>' operator
         let qs2 = vec![
             Query::from_int("foo", QueryCondition::Greater, 1),
         ];
-        if let Some(clause) = t.generate_where_clause(&qs2) {
+        if let Ok(clause) = t.generate_where_clause(&qs2) {
             assert_eq!(clause.clause, "key02 > ?");
-            if let ParameterType::Int(x) = *clause.parameters[0].value {
-                assert_eq!(x, 1);
-            } else {
-                assert!(false);
-            }
+            assert_eq!(*clause.parameters[0].value, ParameterType::Int(1));
+        } else {
+            assert!(false);
+        }
+
+        // test for invalid condition
+        let qs3 = vec![
+            Query::from_int("foo", QueryCondition::None, 1),
+        ];
+        if let Err(err) = t.generate_where_clause(&qs3) {
+            assert_eq!(err, SqlGenerateError::InvalidCondition);
+        } else {
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn test_generate_limit_clause() {
+        let mut t = Table::new("test_table");
+        t.add_key_and_alias("key01", "key01")
+            .add_key_and_alias("key02", "foo");
+
+        let qs1 = vec![
+            Query::from_int("limit", QueryCondition::Equals, 10),
+            Query::from_int("offset", QueryCondition::Equals, 4),
+        ];
+        if let Ok(clause) = t.generate_limit_clause(&qs1) {
+            assert_eq!(clause.clause, "LIMIT ? OFFSET ?");
+            assert_eq!(*clause.parameters[0].value, ParameterType::Int(10));
+            assert_eq!(*clause.parameters[1].value, ParameterType::Int(4));
+        } else {
+            assert!(false);
+        }
+
+    }
+    #[test]
+    fn test_generate_order_by_clause() {
+        let mut t = Table::new("test_table");
+        t.add_key_and_alias("key01", "key01")
+            .add_key_and_alias("key02", "foo");
+
+        // test for 'ASC' order
+        let qs1 = vec![
+            Query::from_str("order_by", QueryCondition::Asc, "key01"),
+        ];
+        if let Ok(clause) = t.generate_order_by_clause(&qs1) {
+            assert_eq!(clause.clause, "key01 ASC");
+        } else {
+            assert!(false);
+        }
+
+        // test for 'ASC' order
+        let qs2 = vec![
+            Query::from_str("order_by", QueryCondition::Desc, "foo"),
+        ];
+        if let Ok(clause) = t.generate_order_by_clause(&qs2) {
+            assert_eq!(clause.clause, "key02 DESC");
         } else {
             assert!(false);
         }
--- src/sql_generator.rs
+++ src/sql_generator.rs
@@ -1,4 +1,7 @@
+use std::error::Error;
+use std::fmt;
 
+#[derive(Debug, PartialEq)]
 pub enum ParameterType {
     Int(i32),
     String(String),
@@ -8,17 +11,34 @@ pub struct Parameter {
     pub value: Box<ParameterType>,
 }
 
-pub struct WhereClause {
+impl Clone for Parameter {
+    fn clone(&self) -> Parameter {
+        Parameter {
+            value: match &*self.value {
+                ParameterType::Int(x)
+                    => Box::new(ParameterType::Int(*x)),
+                ParameterType::String(x)
+                    => Box::new(ParameterType::String(x.to_string())),
+            },
+        }
+    }
+}
+
+pub struct Clause {
     pub clause: String,
     pub parameters: Vec<Parameter>,
 }
 
+#[derive(Debug)]
 pub enum QueryCondition {
     Equals,
     Greater,
     Lesser,
     GreaterEqual,
     LesserEqual,
+    Desc,
+    Asc,
+    None,
 }
 
 pub enum QueryOperator {
@@ -27,7 +47,7 @@ pub enum QueryOperator {
 }
 
 pub struct Query {
-    target_key: String,
+    target: String,
     condition: QueryCondition,
     operator: QueryOperator,
     parameter: Parameter,
@@ -38,11 +58,24 @@ impl Query {
                     cond: QueryCondition,
                     param: i32) -> Query {
         Query {
-            target_key: String::from(target),
+            target: String::from(target),
             condition: cond,
             operator: QueryOperator::And,
             parameter: Parameter {
                 value: Box::new(ParameterType::Int(param)),
+            },
+        }
+    }
+
+    pub fn from_str(target: &str,
+                    cond: QueryCondition,
+                    param: &str) -> Query {
+        Query {
+            target: String::from(target),
+            condition: cond,
+            operator: QueryOperator::And,
+            parameter: Parameter {
+                value: Box::new(ParameterType::String(param.to_string())),
             },
         }
     }
@@ -59,6 +92,35 @@ pub struct Table {
     placeholder: String,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum SqlGenerateError {
+    InvalidCondition,
+}
+
+impl fmt::Display for SqlGenerateError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            SqlGenerateError::InvalidCondition
+                => write!(f, "invalid condition"),
+        }
+    }
+}
+
+impl Error for SqlGenerateError {
+    fn description(&self) -> &str {
+        match *self {
+            SqlGenerateError::InvalidCondition
+                => "invalid condition",
+        }
+    }
+    
+    fn cause(&self) -> Option<&dyn Error> {
+        match *self {
+            SqlGenerateError::InvalidCondition => None,
+        }
+    }
+}
+
 impl Table {
     pub fn new(name: &str) -> Table {
         Table {
@@ -68,7 +130,7 @@ impl Table {
         }
     }
 
-    pub fn add_key(&mut self, name: &str, alias: &str) -> &mut Table {
+    pub fn add_key_and_alias(&mut self, name: &str, alias: &str) -> &mut Table {
         self.keys.push(Key {
             name: String::from(name),
             alias: String::from(alias),
@@ -76,46 +138,96 @@ impl Table {
         self
     }
 
-    fn decode_cond(&self, key: &String, query: &Query) -> String {
+    fn decode_cond(&self, key: &String, query: &Query) -> Result<String, SqlGenerateError> {
         let cond = match query.condition {
             QueryCondition::Equals => "=",
             QueryCondition::Greater => ">",
             QueryCondition::Lesser => "<",
             QueryCondition::GreaterEqual => ">=",
             QueryCondition::LesserEqual => "<=",
+            _ => return Result::Err(SqlGenerateError::InvalidCondition),
         };
-        format!("{} {} {}", key, cond, self.placeholder)
+        Result::Ok(format!("{} {} {}", key, cond, self.placeholder))
     }
 
-    pub fn generate_where_clause(&self, queries: &Vec<Query>) -> Option<WhereClause> {
+    pub fn generate_where_clause(&self, queries: &Vec<Query>)
+                                 -> Result<Clause, SqlGenerateError> {
         let mut clauses: Vec<String> = Vec::new();
         let mut parameters: Vec<Parameter> = Vec::new();
 
         // scan target key
         for q in queries {
-            if let Some(k) = self.keys.iter().find(|&x| x.name == q.target_key) {
-                clauses.push(self.decode_cond(&k.alias, q));
-                let param = Parameter {
-                    value: match &*q.parameter.value {
-                        ParameterType::Int(x)
-                            => Box::new(ParameterType::Int(*x)),
-                        ParameterType::String(x)
-                            => Box::new(ParameterType::String(x.to_string())),
-                    },
-                };
-                parameters.push(param);
+            if let Some(k) = self.keys.iter().find(|&x| x.alias == q.target) {
+                match self.decode_cond(&k.name, q) {
+                    Ok(clause) => clauses.push(clause),
+                    Err(err) => return Err(err),
+                }
+                parameters.push(q.parameter.clone());
             } else {
                 continue;
             }
         }
 
-        if clauses.len() == 0 {
-            return None;
-        }
-        let result = WhereClause {
+        let result = Clause {
             clause: clauses.join(", "),
             parameters: parameters
         };
-        Some(result)
+        Ok(result)
+    }
+
+    pub fn generate_limit_clause(&self, queries: &Vec<Query>)
+                                 -> Result<Clause, SqlGenerateError> {
+        let mut clauses: Vec<String> = Vec::new();
+        let mut parameters: Vec<Parameter> = Vec::new();
+
+        // scan target key
+        for q in queries {
+            if q.target == "limit" {
+                clauses.push(format!("LIMIT {}", self.placeholder));
+                parameters.push(q.parameter.clone());
+                continue;
+            }
+            if q.target == "offset" {
+                clauses.push(format!("OFFSET {}", self.placeholder));
+                parameters.push(q.parameter.clone());
+                continue;
+            }
+        }
+
+        let result = Clause {
+            clause: clauses.join(" "),
+            parameters: parameters
+        };
+        Ok(result)
+    }
+
+    pub fn generate_order_by_clause(&self, queries: &Vec<Query>)
+                                 -> Result<Clause, SqlGenerateError> {
+        let mut clauses: Vec<String> = Vec::new();
+        let parameters: Vec<Parameter> = Vec::new();
+
+        // scan target key
+        for q in queries {
+            if q.target == "order_by" {
+                if let ParameterType::String(key) = &*q.parameter.value {
+                    if let Some(k) = self.keys.iter().find(|x| &x.alias == key) {
+                        let order = match q.condition {
+                            QueryCondition::Asc => "ASC",
+                            QueryCondition::Desc => "DESC",
+                            _ => return Result::Err(SqlGenerateError::InvalidCondition),
+                        };
+                        clauses.push(format!("{} {}", k.name, order));
+                        continue;
+                    }
+                }
+            }
+        }
+
+        let result = Clause {
+            clause: clauses.join(", "),
+            parameters: parameters
+        };
+        Ok(result)
     }
 }
+
